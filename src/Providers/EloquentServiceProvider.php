0c0746b70a67d9e1999ab25759d12cbdf7084400
Patrick <privat@skyraptor.eu>
2022-06-12 23:39:20 +0200

    Create EloquentServiceProvider.php

---
+++ src/Providers/EloquentServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SquadMS\Discord\Providers;
+
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use SquadMS\Foundation\Models\SquadMSUser;
+
+class EloquentServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
