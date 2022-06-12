e3b8a10c988780f623be6cab24e774074331e215
Patrick <privat@skyraptor.eu>
2022-06-13 00:18:53 +0200

    Create sqms-discord.php

---
+++ config/sqms-discord.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  'key' => env('SQMS_DISCORD_BOT_TOKEN'),
+  'permissions' => [
+    'module' => 'sqms-discord',
+    'definitions' => [
+      'admin discord' => 'Grant access to the Discord management',
+    ],
+  ]
+];
